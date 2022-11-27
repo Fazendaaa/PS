257cb29f4c590aeb174c89cc68aa692144e695cd
Fazendaaa <lucas.carotta@outlook.com>
2022-11-27 11:19:40 -0300

    Hide navbar on not signed

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,25 +1,12 @@
 <template>
   <v-img src="@/assets/login.svg"></v-img>
-  <div v-if="logged">
-    <SignIn />
-  </div>
-  <div v-else>
-    <Profile />
-  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import Profile from "./Profile.vue";
-import SignIn from "./SignIn.vue";
 
 export default defineComponent({
   name: "LoginComponent",
-
-  components: {
-    Profile,
-    SignIn,
-  },
 
   setup() {
     return {
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,27 +1,45 @@
 <template>
-  <v-app-bar density="compact" class="hidden-md-and-down">
-    <v-btn v-for="item in items" :key="item.title" :to="item.path">
-      <span>{{ item.title }}</span>
-      <v-icon>{{ item.icon }}</v-icon>
-    </v-btn>
-  </v-app-bar>
-
-  <v-bottom-navigation class="hidden-lg-and-up" grow value="1" color="primary">
-    <v-btn v-for="item in items" :key="item.title" :to="item.path">
-      <span v-html="$vuetify.locale.t(`navbar.${item.title}`)" />
-      <v-icon>{{ item.icon }}</v-icon>
-    </v-btn>
-  </v-bottom-navigation>
+  <div v-if="logged">
+    <v-app-bar density="compact" class="hidden-md-and-down">
+      <v-btn v-for="item in items" :key="item.title" :to="item.path">
+        <span>{{ item.title }}</span>
+        <v-icon>{{ item.icon }}</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <Profile />
+    <v-bottom-navigation
+      class="hidden-lg-and-up"
+      grow
+      value="1"
+      color="primary"
+    >
+      <v-btn v-for="item in items" :key="item.title" :to="item.path">
+        <span v-html="$vuetify.locale.t(`navbar.${item.title}`)" />
+        <v-icon>{{ item.icon }}</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+  </div>
+  <div v-else>
+    <SignIn />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import Profile from "./Profile.vue";
+import SignIn from "./SignIn.vue";
 
 export default defineComponent({
   nome: "NavBarComponent",
 
+  components: {
+    Profile,
+    SignIn,
+  },
+
   setup() {
     return {
+      logged: false,
       items: [
         {
           title: "groups",
