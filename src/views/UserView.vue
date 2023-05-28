4bf0fff1bb8da7aa6f43ea8f16dea8f5532d800c
Fazendaaa <lucas.carotta@outlook.com>
2023-05-28 18:01:29 -0300

    Adding user info retrieval

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -6,36 +6,47 @@
   <br />
 
   <v-expansion-panels variant="inset" class="my-4">
-    <v-expansion-panel>
-      <v-expansion-panel-title>
-        <span>Avatar</span>
-      </v-expansion-panel-title>
+    <v-expansion-panel v-for="(button, index) in buttons" v-bind:key="index">
+      <v-expansion-panel-title v-html="$vuetify.locale.t(`navbar.${button}`)" />
       <v-expansion-panel-text>
-        <div v-for="(button, index) in buttons" v-bind:key="index">
-          <v-btn>
-            <span v-html="$vuetify.locale.t(`navbar.${button}`)" />
-          </v-btn>
-        </div>
-      </v-expansion-panel-text>
-    </v-expansion-panel>
-
-    <v-expansion-panel>
-      <v-expansion-panel-title>
-        <span>Questoes Iniciais</span>
-      </v-expansion-panel-title>
-      <v-expansion-panel-text>
-        <v-select
-          v-model="activity"
-          label="Atividade"
-          :items="items"
-          variant="solo"
-        />
-        <div v-if="activity">
-          <Stopwatch />
-        </div>
+        <v-btn>
+          <span v-html="$vuetify.locale.t(`navbar.${button}`)" />
+        </v-btn>
       </v-expansion-panel-text>
     </v-expansion-panel>
   </v-expansion-panels>
+
+  <div v-if="this.$store.getters.isAdmin">
+    <span>Questoes Iniciais</span>
+
+    <v-expansion-panels variant="inset" class="my-4">
+      <v-expansion-panel>
+        <v-expansion-panel-title>
+          <span>Questoes Iniciais</span>
+        </v-expansion-panel-title>
+        <v-expansion-panel-text>
+          <v-select
+            v-model="activity"
+            label="Atividade"
+            :items="items"
+            variant="solo"
+          />
+          <div v-if="activity">
+            <Stopwatch />
+          </div>
+        </v-expansion-panel-text>
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </div>
+
+  <v-btn block class="my-4">
+    <v-icon>mdi-check</v-icon>
+    <span>Salvar</span>
+  </v-btn>
+  <v-btn block class="my-4">
+    <v-icon>mdi-cancel</v-icon>
+    <span>Cancelar</span>
+  </v-btn>
 </template>
 
 <script lang="ts">
@@ -49,6 +60,21 @@ export default defineComponent({
     const store = useStore();
 
     store.commit("setTheme", "user");
+
+    fetch("https://api.docker.localhost/users/", { method: "POST" })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          console.log("Network response was not ok.");
+        }
+      })
+      .then(console.log)
+      .catch((error) => {
+        console.log(
+          "There has been a problem with your fetch operation: " + error.message
+        );
+      });
 
     return {
       buttons: ["hair", "skin", "clothes", "others"],
