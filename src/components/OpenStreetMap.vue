4f60c396ae5551968931c2159dd147fed8d994c1
Fazendaaa <lucas.carotta@outlook.com>
2023-06-01 10:56:46 -0300

    Adding address API for search

--- src/components/OpenStreetMap.vue
+++ src/components/OpenStreetMap.vue
@@ -27,8 +27,28 @@ export default defineComponent({
   setup() {
     const center = ref([40, 40]);
     const projection = ref("EPSG:4326");
-    const zoom = ref(8);
+    const zoom = ref(18);
     const rotation = ref(0);
+    const address =
+      "Rua Rubens Arruda , 8-50 , Altos da cidade, 17014-300, Bauru".replaceAll(
+        " ",
+        "+"
+      );
+    const url = `http://nominatim.openstreetmap.org/search?q=${address}&format=json&polygon=1&addressdetails=1`;
+
+    fetch(url)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw Error("Response not ok");
+        }
+      })
+      .then((data) => {
+        const address = data[0];
+        center.value = [address["lon"], address["lat"]];
+      })
+      .catch(console.error);
 
     return {
       center,
